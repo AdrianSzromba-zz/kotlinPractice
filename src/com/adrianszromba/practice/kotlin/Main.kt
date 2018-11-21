a213a5fa6db5990d5f626ffaf2ed0341542e943b
AdrianSzromba <adrian.szromba@gmail.com>
2018-11-21 21:39:49 +0100

    declare variables

--- src/com/adrianszromba/practice/kotlin/Main.kt
+++ src/com/adrianszromba/practice/kotlin/Main.kt
@@ -1,5 +1,11 @@
 package com.adrianszromba.practice.kotlin
 
-fun main (args:Array<String>) {
+fun main(args: Array<String>) {
     println("Hello World")
+
+    //declare variables (name before type)
+    var x: Int = 10 //mutable
+    var xx = 10 //in some context, type is optional
+    val y: Int = 10 //immutable
+
 }
