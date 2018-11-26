81f3ef55f810fdb5417fa0c559ef6afc0424da4b
AdrianSzromba <adrian.szromba@gmail.com>
2018-11-26 19:33:14 +0100

    create instance of class

--- src/com/adrianszromba/practice/kotlin/Main.kt
+++ src/com/adrianszromba/practice/kotlin/Main.kt
@@ -8,4 +8,8 @@ fun main(args: Array<String>) {
     var xx = 10 //in some context, type is optional
     val y: Int = 10 //immutable
 
+    val point = Point() //instance of class
+    println(point.x)
+    println(point.y)
+
 }
