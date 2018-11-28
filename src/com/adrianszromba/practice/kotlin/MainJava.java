d0676fa977761f05b7aaa705e045d3dac4818016
AdrianSzromba <adrian.szromba@gmail.com>
2018-11-28 21:10:56 +0100

    What's happen in Java

---
+++ src/com/adrianszromba/practice/kotlin/MainJava.java
@@ -0,0 +1,10 @@
+package com.adrianszromba.practice.kotlin;
+
+public class MainJava {
+	public static void main(String[] args) {
+
+		PointS p1 = new PointS(1,2);
+		System.out.println(p1.getX()); // Kotlin create getter
+		System.out.println(p1.getY());
+	}
+}
