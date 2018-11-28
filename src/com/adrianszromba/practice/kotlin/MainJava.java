ccfbf2fb66810a8285a337885794039a049d8b92
AdrianSzromba <adrian.szromba@gmail.com>
2018-11-28 21:18:09 +0100

    compare Kotlin v Java

--- src/com/adrianszromba/practice/kotlin/MainJava.java
+++ src/com/adrianszromba/practice/kotlin/MainJava.java
@@ -6,5 +6,9 @@ public class MainJava {
 		PointS p1 = new PointS(1,2);
 		System.out.println(p1.getX()); // Kotlin create getter
 		System.out.println(p1.getY());
+
+		PointSJava p2 = new PointSJava(3,4);
+		System.out.println(p2.getX()); // classic Java
+		System.out.println(p2.getY());
 	}
 }
