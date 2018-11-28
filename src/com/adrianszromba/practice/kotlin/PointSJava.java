f055e00bf8e2240b376fb4df1c512f6c10df5618
AdrianSzromba <adrian.szromba@gmail.com>
2018-11-28 21:17:48 +0100

    classic way to create class in java

---
+++ src/com/adrianszromba/practice/kotlin/PointSJava.java
@@ -0,0 +1,20 @@
+package com.adrianszromba.practice.kotlin;
+
+public class PointSJava {
+
+	private int x;
+	private int y;
+
+	public PointSJava(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+}
